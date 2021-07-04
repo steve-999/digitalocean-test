a9654f5eacb980cf60722b38093ce6f1414358a5
steve-999 <steve.green1@gmail.com>
2021-07-04 20:33:20 +0100

    modified server

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express = require('express');
 const cors = require('cors');
-var app = express()
+const path = require('path');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.use(cors());
 
 app.get('/index', function(request, response) {
   response.json('Hello World!')
-})
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
